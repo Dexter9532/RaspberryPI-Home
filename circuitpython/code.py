2f14c7a6f37fcbd0f65c7a585ce79c6efaec2626
Bobo <back-engstrom@hotmail.com>
2025-05-05 23:21:11 +0200

    code.py works okey

--- circuitpython/code.py
+++ circuitpython/code.py
@@ -1,1 +1,30 @@
-print("hej bobo")
+import board
+import displayio
+import terminalio
+from adafruit_display_text import label
+
+display = board.DISPLAY
+main_group = displayio.Group()
+display.root_group = main_group
+
+# Rensa allt
+while len(main_group) > 0:
+    main_group.pop()
+display.refresh()
+
+# Visa exakt det du vill
+text = "Tjena"
+text_area = label.Label(
+    terminalio.FONT,
+    text=text,
+    color=0xFFFFFF,
+    x=10,
+    y=30,
+    scale=2
+)
+main_group.append(text_area)
+display.refresh()
+
+# Håll igång loopen utan att göra något
+while True:
+    pass
